c770d5fca599f193a750fc40327adc281e2a1e2c
Ba5ik7 <dusellfam@gmail.com>
2019-03-21 23:43:43 -0400

    Create Input Field Component

--- src/app/common-components/common-components.module.ts
+++ src/app/common-components/common-components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { InputFieldsComponent } from './input-fields/input-fields.component';
 
 @NgModule({
-  declarations: [ButtonsComponent],
+  declarations: [ButtonsComponent, InputFieldsComponent],
   imports: [
     CommonModule
   ],
