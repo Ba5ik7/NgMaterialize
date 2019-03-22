859db56441b2f0e5ea38fe4d143532959c3e95dc
Ba5ik7 <dusellfam@gmail.com>
2019-03-22 02:20:38 -0400

    Rename button inputs add id the button component

--- src/app/common-components/buttons/buttons.component.ts
+++ src/app/common-components/buttons/buttons.component.ts
@@ -7,18 +7,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ButtonsComponent {
 
+  
+  @Input() buttonClasses: string = 'btn';
+  @Input() buttonId: string;
   @Input() buttonSubmit: boolean = false;
-  @Input() classes: string = 'btn';
+  @Input() buttonText: string;
+
   @Input() icon: string = 'none';
   @Input() iconClasses: string = 'right';
-  @Input() text: string;
 
   @Output() clicked = new EventEmitter<object>();
-
-  constructor() { }
 
   private onClick(event) {
     console.log('Button Clicked', event);
     this.clicked.emit(event);
   }
+
+  constructor() { }
 }
