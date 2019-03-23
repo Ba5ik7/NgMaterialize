a4ff656742bc8fcc88137251be775cb921386732
Wesley <dusellfam@gmail.com>
2019-03-22 21:13:40 -0400

    Feature/radio buttons (#1)
    * Create Radio Buttons component
    
    * Add materialize csss
    
    * Add materialize global styles and display radio component on root
    
    * Setup the HTML and bind the input attributes and add css to display block style
    
    * Add FormControl and FormGroup support for reactive forms

---
+++ src/app/common-components/radio-buttons/radio-buttons.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'md-radio-buttons',
+  templateUrl: './radio-buttons.component.html',
+  styleUrls: ['./radio-buttons.component.scss']
+})
+export class RadioButtonsComponent {
+
+  @Input() formControlName: string = 'test';
+  @Input() control: FormControl = new FormControl();
+  @Input() ratioGroup: FormGroup = new FormGroup({ test: this.control });
+
+  @Input() radios: Array<object> = [{ value: 'test_1', id: 'test_1', text: 'Test One' }, { value: 'test_2', id: 'test_2', text: 'Test Two' }];
+  @Input() ratioClasses: string;
+ 
+  
+  @Input() wrapperClasses: string = 'block';
+
+  private onChange(event) {
+    console.log(event);
+    console.log(this.control);
+    console.log(this.ratioGroup);
+  }
+
+  constructor() { }
+}
