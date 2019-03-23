7be80fcc87752e2f8fec1d96fc4d70fa818f5190
Ba5ik7 <dusellfam@gmail.com>
2019-03-22 22:16:49 -0400

    Add FormControl and FormGroup support for reactive forms

--- src/app/common-components/checkboxes/checkboxes.component.ts
+++ src/app/common-components/checkboxes/checkboxes.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'md-checkboxes',
@@ -7,11 +8,21 @@ import { Component, Input } from '@angular/core';
 })
 export class CheckboxesComponent {
 
+  @Input() formControlName: string = 'test';
+  @Input() control: FormControl = new FormControl();
+  @Input() checkboxesGroup: FormGroup = new FormGroup({ test: this.control });
+
   @Input() checkboxes: Array<object> = [{ id: 'checkboxes_test_1', text: 'Test One' }, { classes: 'filled-in', id: 'checkboxes_test_2', text: 'Test Two' }];
 
-  @Input() labelClasses: string = 'col s2';
+  @Input() labelClasses: string = 'col s12';
 
   @Input() wrapperClasses: string = 'row';
+
+  private onClick(event) {
+    console.log(event);
+    console.log(this.control);
+    console.log(this.checkboxesGroup);
+  }
 
   constructor() { }
 }
