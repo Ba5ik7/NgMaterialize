f3928d6d15176a2c80449bddb58c2b569ffa35e3
Ba5ik7 <dusellfam@gmail.com>
2019-03-23 01:17:25 -0400

    Move input field css to root scss file. Build select wrapper. Add click event to input and document. Add close and open logic to dropdown.

--- src/app/common-components/selects/selects.component.ts
+++ src/app/common-components/selects/selects.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'md-selects',
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 export class SelectsComponent {
 
-  constructor() { }
+  open: boolean = false;
+  listenerFn: Function;
+
+  private onClick(event) {
+    // If select dropdown is not open don't add a new/another document event listener
+    if (this.open !== true) {
+      this.listenerFn = this.renderer.listen('document', 'mousedown', (event) => { this.documentClicked(event); });
+      this.open = true;
+    }
+  }
+
+  private documentClicked(event) {
+    const hasDropdownEl = event.target.closest('.dropdown-content');
+    if (hasDropdownEl == null) {
+      this.open = false;
+      // Remove the event document listener
+      this.listenerFn();
+    }
+  }
+
+  constructor(public renderer: Renderer2) { }
 }
