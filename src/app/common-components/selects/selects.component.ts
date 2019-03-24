295219a31e2ad3ef6526f9f36cfbf3aeca60578c
Ba5ik7 <dusellfam@gmail.com>
2019-03-24 02:31:14 -0400

    On li click event get rid of the DOMStringMap data type by shallow copying and set the data to the formControl

--- src/app/common-components/selects/selects.component.ts
+++ src/app/common-components/selects/selects.component.ts
@@ -15,7 +15,7 @@ export class SelectsComponent {
   labelText: string = 'Materialize Select';
 
   options = [
-    {text: 'Arizona', id: 'AZ'},
+    {text: 'Arizona', id: 'AZ', test: 'test'},
     {text: 'California', id: 'CA'},
     {text: 'Colorado', id: 'CO'},
     {text: 'New York', id: 'NY'},
@@ -28,17 +28,19 @@ export class SelectsComponent {
 
 
   private optionClick(event) {
-    console.log('optionClick', event);
-    // Close the dropdown
-    this.open = false;
+    // Get rid of the DOMStringMap shallow copying and set the data to the formControl
+    const test = Object.assign({}, event.target.dataset);
+    this.form.setValue({ test });
+
     // Remove the event document listener
     this.listenerFn();
+    this.open = false;
   }
 
   private onClick(event) {
     // If select dropdown is not open don't add a new/another document event listener
     if (this.open !== true) {
-      this.listenerFn = this.renderer.listen('document', 'mousedown', (event) => { this.documentClicked(event); });
+      this.listenerFn = this.renderer.listen('document', 'mousedown', (event) => this.documentClicked(event));
       this.open = true;
     }
   }
